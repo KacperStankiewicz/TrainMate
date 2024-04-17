27e751e1e54f51327cb6d044ea4b9ad4b4af5f98
KStankiewicz <s22619@pjwstk.edu.pl>
2024-04-17 21:55:16 +0200

    hotfix - no auditing

--- core/src/main/java/pl/edu/pja/trainmate/core/CoreApplication.java
+++ core/src/main/java/pl/edu/pja/trainmate/core/CoreApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @OpenAPIDefinition(
     info = @Info(
@@ -15,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
     servers = @Server(url = "/api/trainmate")
 )
 @SpringBootApplication
+@EnableJpaAuditing
 public class CoreApplication {
 
     public static void main(String[] args) {
