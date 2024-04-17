0e61d6a4e550a75a21fcd67971561f24fd0b48d4
Bartosz Laskowski <s23046@pjwstk.edu.pl>
2024-04-17 20:54:44 +0200

    Revert "Not working exwercise"
    This reverts commit fc14ed23464451e677df9941b6127f7f65f840c4.
    
    	deleted:    core/src/main/java/pl/edu/pja/trainmate/core/api/ExerciseController.java
    	modified:   core/src/main/java/pl/edu/pja/trainmate/core/api/MenteeController.java
    	deleted:    core/src/main/java/pl/edu/pja/trainmate/core/api/WorkoutController.java
    	deleted:    core/src/main/java/pl/edu/pja/trainmate/core/domain/workoutplan/exercise/ExerciseFacade.java
    	deleted:    core/src/main/java/pl/edu/pja/trainmate/core/domain/workoutplan/exercise/ExerciseRepository.java
    	deleted:    core/src/main/java/pl/edu/pja/trainmate/core/domain/workoutplan/exercise/ExerciseService.java
    	deleted:    core/src/main/java/pl/edu/pja/trainmate/core/domain/workoutplan/exercise/dto/ExerciseCreateDto.java
    	deleted:    core/src/main/java/pl/edu/pja/trainmate/core/domain/workoutplan/exercise/mapper/ExerciseMapper.java
    	modified:   core/src/main/resources/application.yml
    	modified:   core/src/main/java/pl/edu/pja/trainmate/core/CoreApplication.java

--- core/src/main/java/pl/edu/pja/trainmate/core/domain/workoutplan/exercise/ExerciseRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pja.trainmate.core.domain.workoutplan.exercise;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExerciseRepository extends JpaRepository<ExerciseEntity, Long> {
-
-}
