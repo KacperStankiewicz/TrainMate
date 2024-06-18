8b14974fe2563e82831236b862a965dece9dbf89
KStankiewicz <s22619@pjwstk.edu.pl>
2024-06-19 00:04:22 +0200

    TrainMate-65 personal trainer get files

--- core/src/main/java/pl/edu/pja/trainmate/core/common/error/ReportErrorCode.java
+++ core/src/main/java/pl/edu/pja/trainmate/core/common/error/ReportErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ReportErrorCode implements BaseErrorCode {
 
     COULD_NOT_CREATE_REPORT("Could not create report", BAD_REQUEST),
-    EXERCISE_WAS_ALREADY_REPORTED("Can not create new report for exercise that was already reported", BAD_REQUEST),
+    CAN_NOT_CHANGE_REVIEWED_REPORT("Can not change reviewed exercise report", BAD_REQUEST),
     EXERCISE_WAS_NOT_REPORTED("Can not review report if exercise is not reported", BAD_REQUEST),
     WORKOUT_PLAN_WAS_ALREADY_REPORTED("Report for workout plan with id {} already exists", BAD_REQUEST),
     INITIAL_REPORT_ALREADY_EXISTS("Initial report already exists", BAD_REQUEST);
